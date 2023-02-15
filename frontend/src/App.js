ffcef3c83734a9a5035deadf82b938f9f317200a
Jhon Tafur <jhontamos@hotmail.com>
2023-02-15 08:36:02 -0500

    Iniciando el proyecto

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">TIF Store</a>
+      </header>
+      <main> Hola mundo </main>
+    </div>
+  );
+}
+
+export default App;
